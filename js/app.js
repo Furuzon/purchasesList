ae0c9977abf3f257d8e3d0e6149901b0fa0101a8
furuzon <furuzon@gmailmadaliev.com>
2020-01-20 21:53:21 -0800

    PurchasesList

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const rootEl = document.getElementById('root')
+
+rootEl.innerHTML = `
+<form data-id="purchase-add-form">
+<label for="purchase-input-amount">Summa</label>
+<input id="purchase-input-amount" data-id="purchase-input-amount">
+<label for="purchase-input-category">Category</label>
+<input id="purchase-input-category" data-id="purchase-input-category">
+<button data-action="purchase-add">Dobavit</button>
+</form>
+<div data-id="purchases-total">0</div>
+`
+
+const purchaseAddFormEl = rootEl.querySelector('[data-id=purchase-add-form]')
+
+const purchaseInputAmounEl = purchaseAddFormEl.querySelector('[data-id=purchase-input-amoun]')
+
+const purchaseInputCategoryEl = purchaseAddFormEl.querySelector('[data-id=purchase-input-category]')
+const purchaseAddButtonEl = purchaseAddFormEl.querySelector('[data-action=purchase-add]')
+
+purchaseAddButtonEl.onclick = evt => {
+  evt.preventDefault()
+}
